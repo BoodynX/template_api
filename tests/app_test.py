b0be8d2d5e82b6b7cbc76f457821bf9cb74930c7
Tomasz Budny <budny.tomasz@gmail.com>
2019-06-05 17:03:08 +0200

    tests: add setUpClass and tearDown for test.db support to AppTest

--- tests/app_test.py
+++ tests/app_test.py
@@ -1,9 +1,25 @@
-from tests.base_test import BaseTest
-
+from app.framework.db import db
 from run import flask
+from tests.base_test import BaseTest
 
 
 class AppTest(BaseTest):
+    SQLALCHEMY_DATABASE_URI = "sqlite:///test.db"
+
+    @classmethod
+    def setUpClass(cls):
+        flask.config['SQLALCHEMY_DATABASE_URI'] = AppTest.SQLALCHEMY_DATABASE_URI
+        flask.config['DEBUG'] = False
+        with flask.app_context():
+            db.init_app(flask)
+
     def setUp(self):
+        with flask.app_context():
+            db.create_all()
         self.app_client = flask.test_client
         self.app_context = flask.app_context
+
+    def tearDown(self):
+        with flask.app_context():
+            db.session.remove()
+            db.drop_all()
