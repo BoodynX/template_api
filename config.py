5f16424e2444e1e87c2614b5d41519b2660d6218
Tomasz Budny <budny.tomasz@gmail.com>
2019-06-11 07:03:22 +0200

    env: add DATABASE_URL to the .env file

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 DEBUG = os.environ.get("DEBUG")
-SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///data.db")
+SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///default.db")
 
 # This setting turns off the Flask-SQLAlchemy changes tracker,
 # because the basic SQLAlchemy tracker works better (more efficiently)
