281f725f77c78e8ef3df87edadeae22660876aef
Tomasz Budny <budny.tomasz@gmail.com>
2019-06-15 00:09:31 +0200

    refactor: TestUserRegistrationSchema extract string cost to global

--- tests/unit/test_user_registration_schema.py
+++ tests/unit/test_user_registration_schema.py
@@ -2,14 +2,17 @@ from app.entities.user import User
 from app.schemas.user_registration_schema import UserRegistrationSchema
 from tests.app_test import AppTest
 
+USER_NAME = 'usernameTestUserRegistrationSchema'
+PASSWORD = 'passwordTestUserRegistrationSchema'
+
 
 class TestUserRegistrationSchema(AppTest):
 
     def test_registration_schema(self):
-        registration_form = {'username': 'Test Username', 'password': 'Test Password'}
+        registration_form = {'username': USER_NAME, 'password': PASSWORD}
         user_post_schema = UserRegistrationSchema()
 
         registration = user_post_schema.load(registration_form)
 
         self.assertIsInstance(registration, User)
-        self.assertEqual(registration.username, 'Test Username')
+        self.assertEqual(registration.username, USER_NAME)
