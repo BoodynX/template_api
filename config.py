36d7273fd8f18109e46006ec747fa1340d31f82a
Tomasz Budny <budny.tomasz@gmail.com>
2019-06-14 23:35:43 +0200

    config: set PROPAGATE_EXCEPTIONS = True

--- config.py
+++ config.py
@@ -6,3 +6,7 @@ SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///default.db")
 # This setting turns off the Flask-SQLAlchemy changes tracker,
 # because the basic SQLAlchemy tracker works better (more efficiently)
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# This will bubble the ValidationErrors to tun.py,
+# so we can have one try/catch there for handling them
+PROPAGATE_EXCEPTIONS = True
