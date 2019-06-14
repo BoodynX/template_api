782dd2945266c0885e4083d936fb53acfde39ebc
Tomasz Budny <budny.tomasz@gmail.com>
2019-06-15 00:07:43 +0200

    refactor: UserRepository return None if record not found

--- app/repositories/user.py
+++ app/repositories/user.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 from app.entities.user import User
 from app.framework.database import db
 from app.models.user import UserModel
@@ -13,15 +15,17 @@ class UserRepository:
         db.session.commit()
 
     @classmethod
-    def find_by_username(cls, username: str) -> User:
+    def find_by_username(cls, username: str) -> Union[User, None]:
         user = cls._model.query.filter_by(username=username).first()
         return cls._make_entity(user)
 
     @classmethod
-    def find_by_id(cls, _id: int) -> User:
+    def find_by_id(cls, _id: int) -> Union[User, None]:
         user = cls._model.query.filter_by(id=_id).first()
         return cls._make_entity(user)
 
     @classmethod
-    def _make_entity(cls, user: UserModel) -> User:
+    def _make_entity(cls, user: UserModel) -> Union[User, None]:
+        if not user:
+            return None
         return User(id_=user.id, username=user.username, password=user.password)
