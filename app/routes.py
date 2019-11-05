2735c797db4c3527477db5828c252160d90e8b8c
Tomasz Budny <budny.tomasz@gmail.com>
2019-11-05 07:13:43 +0100

    refactor: separate production routes to a method

--- app/routes.py
+++ app/routes.py
@@ -13,13 +13,15 @@ class Routes:
     @classmethod
     def load(cls, flask: Flask):
         api = Api(flask)
-
-        api.add_resource(Home, cls.HOME)
-        api.add_resource(UsersResource, cls.USER_REGISTRATION)
-
+        cls.load_production_routes(api)
         if flask.config['ENV'] == 'development':
             # this is set as FLASK_ENV in the .env
             cls.load_test_routes(flask)
+
+    @classmethod
+    def load_production_routes(cls, api: Api):
+        api.add_resource(Home, cls.HOME)
+        api.add_resource(UsersResource, cls.USER_REGISTRATION)
 
     @classmethod
     def load_test_routes(cls, flask):
