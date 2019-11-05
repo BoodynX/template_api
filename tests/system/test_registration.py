45fffca664079c80aa8240ff4e3cb39b68921cd8
Tomasz Budny <budny.tomasz@gmail.com>
2019-11-05 07:33:26 +0100

    tests: registration response payload asserts

--- tests/system/test_registration.py
+++ tests/system/test_registration.py
@@ -25,18 +25,75 @@ class TestRegistration(AppTest):
             with self.app_context():
                 response = self.register_user(client, '', PASSWORD)
                 self.assertEqual(400, response.status_code)
+                self.assertDictEqual(
+                    {
+                        "error": "invalid_parameters",
+                        "parameters": {
+                            "username": [
+                                "Invalid value."
+                            ]
+                        }
+                    },
+                    json.loads(response.data)
+                )
 
     def test_submit_for_an_account_with_empty_password(self):
         with self.app_client() as client:
             with self.app_context():
                 response = self.register_user(client, USER_NAME, '')
                 self.assertEqual(400, response.status_code)
+                self.assertDictEqual(
+                    {
+                        "error": "invalid_parameters",
+                        "parameters": {
+                            "password": [
+                                "Invalid value."
+                            ]
+                        }
+                    },
+                    json.loads(response.data)
+                )
 
     def test_submit_for_an_account_with_invalid_username(self):
         with self.app_client() as client:
             with self.app_context():
                 response = self.register_user(client, 'Some strange characters -> $%^&*)_>+', PASSWORD)
                 self.assertEqual(400, response.status_code)
+                self.assertDictEqual(
+                    {
+                        "error": "invalid_parameters",
+                        "parameters": {
+                            "username": [
+                                "Invalid value."
+                            ]
+                        }
+                    },
+                    json.loads(response.data)
+                )
+
+    def test_submit_for_an_account_with_missing_form_fields(self):
+        with self.app_client() as client:
+            with self.app_context():
+                response = client.post(
+                    Routes.USER_REGISTRATION,
+                    data=json.dumps({}),
+                    headers={'Content-Type': 'application/json'}
+                )
+                self.assertEqual(400, response.status_code)
+                self.assertDictEqual(
+                    {
+                        "error": "invalid_parameters",
+                        "parameters": {
+                            "password": [
+                                "Missing data for required field."
+                            ],
+                            "username": [
+                                "Missing data for required field."
+                            ]
+                        }
+                    },
+                    json.loads(response.data)
+                )
 
     def register_user(self, client, username, password):
         return client.post(
